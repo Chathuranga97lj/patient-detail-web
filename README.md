3ce11efef1ce10e8ace4cd2675ebab37d9aec355
SasithDuleepa <duleepajayathissa@gmail.com>
2023-02-09 02:26:05 +0530

    folders create

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Patient Data Recognition Application
+
+npm i react-router-dom@5.2.0
+npm install react-icons --save
--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import Navbar from './components/navbar/navbar';
+import Person from './components/navbar/person/person';
+
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import {Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h>patient detail</h>
-    </div>
+   /* <Router>
+      <Switch>
+        <Route path="/navbar" exact><Navbar/></Route>
+
+      </Switch>
+    </Router>
+*/
+
+<div>
+  <Navbar/>
+
+  <div className='frame_main'>
+    <Person/>
+
+  </div>
+</div>
+
+    
     
   );
 }
